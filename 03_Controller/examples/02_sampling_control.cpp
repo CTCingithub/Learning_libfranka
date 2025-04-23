9216296a6f2c78bc16708bb30a6aca0418701c54
CTC <2801320287@qq.com>
2025-04-23 19:25:46 +0800

    Updated on 2025.04.23

--- 03_Controller/examples/02_sampling_control.cpp
+++ 03_Controller/examples/02_sampling_control.cpp
@@ -6,54 +6,38 @@
 #include "yaml-cpp/yaml.h"
 #include <Eigen/Dense>
 
-#include <franka/duration.h>
+#include <franka/active_control.h>
+#include <franka/active_motion_generator.h>
 #include <franka/exception.h>
 #include <franka/model.h>
 #include <franka/robot.h>
-#include <franka/robot_state.h>
 
+#include "Controllers.h"
 #include "DefaultBehavior.h"
 #include "EnableArrayPrint.h"
 #include "ReadConfig.h"
 
 struct sampled_states_struct {
-  std::array<double, 7> q_sampled;
-  std::array<double, 7> dq_sampled;
   franka::RobotState state_sampled;
-  std::array<double, 7> torques;
 };
 
-std::array<double, 7> PD_Feedback_Func(
-    const franka::Model &model, const franka::RobotState &sampled_state,
-    const std::array<double, 7> &Kp, const std::array<double, 7> &Kd,
-    const std::array<double, 7> &q_sampled,
-    const std::array<double, 7> &dq_sampled) {
-  Eigen::Map<const Eigen::Matrix<double, 7, 1>> q_desired(
-      sampled_state.q.data());
-  Eigen::Map<Eigen::Matrix<double, 7, 7>> mass_matrix_desired(
-      model.mass(sampled_state).data());
-  Eigen::Map<Eigen::Matrix<double, 7, 1>> gravity_compensation(
-      model.gravity(sampled_state).data());
-  Eigen::Matrix<double, 7, 1> e_sampled;
-  for (size_t i = 0; i < 7; ++i) {
-    e_sampled[i] = q_sampled[i] - q_desired[i];
+namespace {
+std::array<double, 7>
+CosineWaveTrajectory(const double time, const std::array<double, 7> &Amplitudes,
+                     const std::array<double, 7> &Omegas) {
+  /*
+  Cosine-wave trajectory, q_i(t) = q_i(0) + A_i * (1 - cos(omega_i * t)
+  Velocity follows \dot{q}_i(t) = -A_i * omega_i * sin(omega_i * t)
+  Use `JointVelocities` mode to eliminate initial position inputs
+  */
+  constexpr double dt = 0.001;
+  std::array<double, 7> velocity; // [rad/s]
+  for (int i = 0; i < 7; i++) {
+    velocity[i] = -Amplitudes[i] * Omegas[i] * sin(Omegas[i] * time);
   }
-  Eigen::Map<const Eigen::Matrix<double, 7, 1>> de_sampled(dq_sampled.data());
-  Eigen::DiagonalMatrix<double, 7> Kp_matrix(
-      Eigen::Map<const Eigen::Matrix<double, 7, 1>>(Kp.data()));
-  Eigen::DiagonalMatrix<double, 7> Kd_matrix(
-      Eigen::Map<const Eigen::Matrix<double, 7, 1>>(Kd.data()));
-  Eigen::Matrix<double, 7, 1> tau =
-      gravity_compensation - Kp_matrix * e_sampled - Kd_matrix * de_sampled;
-
-  // std::array<double, 7> tau_array;
-  // Eigen::MatrixXd::Map(tau_array.data(), 7) = tau;
-  std::array<double, 7> tau_array;
-  Eigen::Map<Eigen::Matrix<double, 7, 1>> tau_map(tau_array.data());
-  tau_map = tau;
-
-  return tau_array;
+  return velocity;
 }
+} // anonymous namespace
 
 int main(int argc, char **argv) {
   // Read config file
@@ -66,57 +50,89 @@ int main(int argc, char **argv) {
             << std::endl;
   std::array<double, 7> q_desired =
       controller_config["Desired_Pos"].as<std::array<double, 7>>();
-  std::array<double, 7> e_initial =
-      controller_config["Initial_Error"].as<std::array<double, 7>>();
   std::array<double, 7> Kp =
       controller_config["Kp"].as<std::array<double, 7>>();
   std::array<double, 7> Kd =
       controller_config["Kd"].as<std::array<double, 7>>();
-  std::array<double, 7> q_initial;
+  std::array<double, 7> amplitudes =
+      controller_config["Amplitude"].as<std::array<double, 7>>();
+  std::array<double, 7> omegas =
+      controller_config["Omega"].as<std::array<double, 7>>();
 
-  // Generate initial position
-  for (size_t i = 0; i < q_desired.size(); i++) {
-    q_initial[i] = q_desired[i] + e_initial[i];
-  }
   const int ts = controller_config["Sampling_Time"].as<int>();
+  const double t_end = controller_config["End_Time"].as<double>();
   std::cout << "Sampling time: " << ts << " ms" << std::endl;
+
+  // Parameters
+  const size_t joint_number{4};
+  const size_t filter_size{1};
+
+  // NOLINTNEXTLINE(readability-identifier-naming)
+  const std::array<double, 7> K_P{
+      {200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0}};
+  // NOLINTNEXTLINE(readability-identifier-naming)
+  const std::array<double, 7> K_D{{10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 10.0}};
+  const double max_acceleration{1.0};
+
+  Controllers::PDController pd_controller(filter_size, K_P, K_D);
 
   try {
     franka::Robot Myrobot(robot_ip);
-    Reach_Desired_Joint_Pose(Myrobot, q_desired);
-    franka::RobotState sampled_state = Myrobot.readOnce();
-    Reach_Desired_Joint_Pose(Myrobot);
-
-    std::cout << "Moving to initial position" << std::endl;
-    Reach_Desired_Joint_Pose(Myrobot, q_initial);
-    franka::RobotState initial_state = Myrobot.readOnce();
     franka::Model Mymodel(Myrobot.loadModel());
-    std::cout << "Reached initial position" << std::endl;
 
-    int time = 0;
-    sampled_states_struct SampledStruct;
-    SampledStruct.q_sampled = initial_state.q;
-    SampledStruct.dq_sampled = initial_state.dq;
-    SampledStruct.state_sampled = initial_state;
-    SampledStruct.torques = initial_state.tau_J;
+    Reach_Desired_Joint_Pose(Myrobot, q_desired);
+    set_default_collision(Myrobot);
 
-    std::function<franka::Torques(const franka::RobotState &, franka::Duration)>
-        controller_callback = [&](const franka::RobotState &robot_state,
-                                  franka::Duration period) -> franka::Torques {
-      time += period.toMSec();
-      if (time % ts == 0) {
-        SampledStruct.q_sampled = robot_state.q;
-        SampledStruct.dq_sampled = robot_state.dq;
-        SampledStruct.state_sampled = robot_state;
-        SampledStruct.torques = robot_state.tau_J;
+    double t = 0.0;
+    int timer = 0;
+    franka::RobotState robot_state_sampled = Myrobot.readOnce();
+
+    auto callback_control = [&](const franka::RobotState &robot_state,
+                                franka::Duration period) -> franka::Torques {
+      // Manual switch sampling frequency
+      timer += period.toMSec();
+      if (timer % ts == 0) {
+        robot_state_sampled = robot_state;
+        std::cout << "Sample at " << timer << "ms" << std::endl;
       }
-      return PD_Feedback_Func(Mymodel, SampledStruct.state_sampled, Kp, Kd,
-                              SampledStruct.q_sampled,
-                              SampledStruct.dq_sampled);
+      return pd_controller.step(robot_state_sampled, Mymodel);
     };
-    Myrobot.control(controller_callback);
+
+    auto callback_motion_generator =
+        [&](const franka::RobotState &,
+            franka::Duration period) -> franka::JointVelocities {
+      t += period.toSec();
+      franka::JointVelocities velocities{{0, 0, 0, 0, 0, 0, 0}};
+      auto velocity_array = CosineWaveTrajectory(t, amplitudes, omegas);
+      for (size_t i = 0; i < 7; i++) {
+        velocities.dq[i] = velocity_array[i];
+      }
+      if (t >= t_end) {
+        return franka::MotionFinished(velocities);
+      }
+      return velocities;
+    };
+
+    bool motion_finished = false;
+    auto active_control = Myrobot.startJointVelocityControl(
+        research_interface::robot::Move::ControllerMode::kExternalController);
+
+    while (!motion_finished) {
+      auto read_once_return = active_control->readOnce();
+      auto robot_state = read_once_return.first;
+      auto duration = read_once_return.second;
+      auto joint_velocities = callback_motion_generator(robot_state, duration);
+      auto torques = callback_control(robot_state, duration);
+      motion_finished = joint_velocities.motion_finished;
+      active_control->writeOnce(joint_velocities, torques);
+    }
+
+  } catch (const franka::ControlException &e) {
+    std::cout << e.what() << std::endl;
+    return -1;
   } catch (const franka::Exception &e) {
     std::cout << e.what() << std::endl;
+    return -1;
   }
 
   return 0;
