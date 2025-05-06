5304ebce565e6426897a8c2398534fca4077e9fe
CTC <2801320287@qq.com>
2025-05-06 22:40:48 +0800

    Updated on 2025.05.06

--- 03_Controller/examples/02_sampling_control.cpp
+++ 03_Controller/examples/02_sampling_control.cpp
@@ -13,6 +13,7 @@
 #include <franka/robot.h>
 
 #include "Controllers.h"
+#include "CosineWaveGenerator.h"
 #include "DefaultBehavior.h"
 #include "EnableArrayPrint.h"
 #include "ReadConfig.h"
@@ -22,24 +23,6 @@ struct state_logger {
   std::array<double, 7> dq;
   std::array<double, 7> tau;
 };
-
-namespace {
-std::array<double, 7>
-CosineWaveTrajectory(const double time, const std::array<double, 7> &Amplitudes,
-                     const std::array<double, 7> &Omegas) {
-  /*
-  Cosine-wave trajectory, q_i(t) = q_i(0) + A_i * (1 - cos(omega_i * t)
-  Velocity follows \dot{q}_i(t) = -A_i * omega_i * sin(omega_i * t)
-  Use `JointVelocities` mode to eliminate initial position inputs
-  */
-  constexpr double dt = 0.001;
-  std::array<double, 7> velocity; // [rad/s]
-  for (int i = 0; i < 7; i++) {
-    velocity[i] = -Amplitudes[i] * Omegas[i] * sin(Omegas[i] * time);
-  }
-  return velocity;
-}
-} // anonymous namespace
 
 int main(int argc, char **argv) {
   // Read config file
@@ -62,11 +45,13 @@ int main(int argc, char **argv) {
       controller_config["Omega"].as<std::array<double, 7>>();
   bool coriolis_compensation =
       controller_config["Coriolis_Compensation"].as<bool>();
-  std::cout << "Compensation:\n"
-            << "Coriolis: " << coriolis_compensation << std::endl;
+  std::cout << "Coriolis Compensation: " << coriolis_compensation << std::endl;
+  bool sampling_info = controller_config["Sampling_Info"].as<bool>();
+  std::cout << "Sampling Info: " << sampling_info << std::endl;
 
   const int ts = controller_config["Sampling_Time"].as<int>();
-  const double t_end = controller_config["End_Time"].as<double>();
+  const double t_switch = controller_config["Switch_Time"].as<double>();
+  const double t_back = controller_config["Backing_Time"].as<double>();
   std::cout << "Sampling time: " << ts << " ms" << std::endl;
 
   // Parameters
@@ -78,6 +63,7 @@ int main(int argc, char **argv) {
   try {
     franka::Robot Myrobot(robot_ip);
     franka::Model Mymodel(Myrobot.loadModel());
+    Myrobot.automaticErrorRecovery();
 
     Reach_Desired_Joint_Pose(Myrobot);
     Reach_Desired_Joint_Pose(Myrobot, q_desired);
@@ -87,51 +73,39 @@ int main(int argc, char **argv) {
     int timer = 0;
     franka::RobotState robot_state_sampled = Myrobot.readOnce();
 
-    auto callback_control = [&](const franka::RobotState &robot_state,
-                                franka::Duration period) -> franka::Torques {
+    auto callback_control_torque =
+        [&](const franka::RobotState &robot_state,
+            franka::Duration period) -> franka::Torques {
       // Manual switch sampling frequency
       timer += period.toMSec();
       if (timer % ts == 0) {
         robot_state_sampled = robot_state;
-        std::cout << "Sample at " << timer << "ms" << std::endl;
+        if (sampling_info) {
+          std::cout << "Sample at " << timer << "ms" << std::endl;
+        }
       }
       return pd_controller.step(robot_state_sampled, Mymodel);
     };
 
-    auto callback_motion_generator =
-        [&](const franka::RobotState &,
-            franka::Duration period) -> franka::JointVelocities {
-      t += period.toSec();
-      franka::JointVelocities velocities{{0, 0, 0, 0, 0, 0, 0}};
-      auto velocity_array = CosineWaveTrajectory(t, amplitudes, omegas);
-      for (size_t i = 0; i < 7; i++) {
-        velocities.dq[i] = velocity_array[i];
-      }
-      if (t >= t_end) {
-        return franka::MotionFinished(velocities);
-      }
-      return velocities;
-    };
+    CosineWaveMotionGenerator motion_generator(robot_state_sampled.q,
+                                               amplitudes, omegas, t_switch);
 
     bool motion_finished = false;
-    auto active_control = Myrobot.startJointVelocityControl(
+    auto active_control = Myrobot.startJointPositionControl(
         research_interface::robot::Move::ControllerMode::kExternalController);
 
     while (!motion_finished) {
       auto read_once_return = active_control->readOnce();
       auto robot_state = read_once_return.first;
       auto duration = read_once_return.second;
-      auto joint_velocities = callback_motion_generator(robot_state, duration);
-      auto torques = callback_control(robot_state, duration);
-      motion_finished = joint_velocities.motion_finished;
-      active_control->writeOnce(joint_velocities, torques);
+      auto joint_positions = motion_generator(robot_state, duration);
+      auto torques = callback_control_torque(robot_state, duration);
+      motion_finished = joint_positions.motion_finished;
+      active_control->writeOnce(joint_positions, torques);
     }
-
+    // Myrobot.control(motion_generator);
+    Myrobot.automaticErrorRecovery();
     Reach_Desired_Joint_Pose(Myrobot);
-
-  } catch (const franka::ControlException &e) {
-    std::cout << e.what() << std::endl;
-    return -1;
   } catch (const franka::Exception &e) {
     std::cout << e.what() << std::endl;
     return -1;
